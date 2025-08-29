a45a31a7ab61f19f634725c8971e9c69c95d868c
xingen <335728491@qq.com>
2025-08-29 09:46:37 +0800

    1

--- src/views/home/components/center/tree.vue
+++ src/views/home/components/center/tree.vue
@@ -381,11 +381,11 @@ watch(checkedKeys, (val) => {
 
 // -------------------- 初始化 --------------------
 onMounted(async () => {
-  const data = await getShequTreeData();
+  // const data = await getShequTreeData();
   // 生成唯一key
-  // const transformed =  generateCommunities() 
-   const transformed =  transformCommunityTreeAutoKey3D(data);
-  tableData.value = data.communityVillage
+  const transformed =  generateCommunities() 
+  //  const transformed =  transformCommunityTreeAutoKey3D(data);
+  // tableData.value = data.communityVillage
   treeData.value = transformed;
   originalTreeData.value = transformed; // 保存原始树数据
   expandedKeys.value = getAllKeys(transformed);
